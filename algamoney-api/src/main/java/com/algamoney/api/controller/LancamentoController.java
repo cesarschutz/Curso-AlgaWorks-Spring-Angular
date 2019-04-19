7ce39c894080e90c302c8b65a4a963c6076f151b
cesarschutz <cesarschutz340@gmail.com>
2019-04-19 16:01:34 -0300

    5.6 - Regra para não salvar pessoa inativa

--- algamoney-api/src/main/java/com/algamoney/api/controller/LancamentoController.java
+++ algamoney-api/src/main/java/com/algamoney/api/controller/LancamentoController.java
@@ -1,14 +1,19 @@
 package com.algamoney.api.controller;
 
+import java.util.Arrays;
 import java.util.List;
 
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
+import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,8 +22,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.algamoney.api.event.RecursoCriadoEvent;
+import com.algamoney.api.exceptionHandler.AlgaMoneyExceptionHanlder.Erro;
 import com.algamoney.api.model.Lancamento;
 import com.algamoney.api.repository.LancamentoRepository;
+import com.algamoney.api.service.LancamentoService;
+import com.algamoney.api.service.exception.PessoaInexistenteOuInativaException;
 
 @RestController
 @RequestMapping("/lancamentos")
@@ -28,7 +36,13 @@ public class LancamentoController {
 	private LancamentoRepository lancamentoRepository;
 	
 	@Autowired
+	LancamentoService lancamentoService;
+	
+	@Autowired
 	private ApplicationEventPublisher publisher;
+	
+	@Autowired
+	MessageSource messageSource;
 	
 	@GetMapping
 	public List<Lancamento> listar() {
@@ -43,9 +57,18 @@ public class LancamentoController {
 	
 	@PostMapping
 	public ResponseEntity<Lancamento> criar(@Valid @RequestBody Lancamento lancamento, HttpServletResponse response) {
-		Lancamento lancamentoSalvo = lancamentoRepository.save(lancamento);
+		Lancamento lancamentoSalvo = lancamentoService.salvar(lancamento);
+		
 		publisher.publishEvent(new RecursoCriadoEvent(this, response, lancamentoSalvo.getCodigo()));
+		
 		return ResponseEntity.status(HttpStatus.CREATED).body(lancamentoSalvo);
 	}
 	
+	@ExceptionHandler({PessoaInexistenteOuInativaException.class})
+	public ResponseEntity<Object> handlePessoaInexistenteOuInativaException(PessoaInexistenteOuInativaException exception){
+		String mensagemUsuario = messageSource.getMessage("pessoa.inexistente-ou-inativa", null, LocaleContextHolder.getLocale());
+		String mensagemDesenvolvedor = exception.toString();
+		List<Erro> erros = Arrays.asList(new Erro(mensagemUsuario, mensagemDesenvolvedor));
+		return ResponseEntity.badRequest().body(erros);
+	}	
 }
