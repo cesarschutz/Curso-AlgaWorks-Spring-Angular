d0b1ead17a4a40dc0d0bd87cb600aa69ddbc9633
cesarschutz <cesarschutz340@gmail.com>
2019-04-27 00:29:23 -0300

    11.5. Exportando a API da diretiva para o template

--- algamoney-ui/algamoney-ui/src/app/campo-colorido.directive.ts
+++ algamoney-ui/algamoney-ui/src/app/campo-colorido.directive.ts
@@ -1,7 +1,8 @@
 import { Directive, HostListener, HostBinding, Input } from '@angular/core';
 
 @Directive({
-  selector: '[appCampoColorido]'
+  selector: '[appCampoColorido]',
+  exportAs: 'campoColorido'
 })
 export class CampoColoridoDirective {
 
@@ -12,12 +13,12 @@ export class CampoColoridoDirective {
   constructor() {}
 
   @HostListener('focus')
-  aoGanharFoco() {
+  colorir() {
     this.cordDeFundo = this.cor;
   }
 
   @HostListener('blur')
-  aoPerderFoco() {
+  descolorir() {
     this.cordDeFundo = 'transparent';
   }
 }
