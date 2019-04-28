07510c31969f542d950454b0ab58befed4397983
cesarschutz <cesarschutz340@gmail.com>
2019-04-28 20:31:10 -0300

    13.9. Criando componente de mensagem de erro de validação

---
+++ algamoney-ui/algamoney-ui/src/app/message/message.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { FormControl } from '@angular/forms';
+
+@Component({
+  selector: 'app-message',
+  template: `
+  <p-message severity="error"
+    *ngIf="temErro()"
+    text="{{text}}">
+  </p-message>
+  `,
+  styles: []
+})
+export class MessageComponent {
+
+  @Input() error: string;
+  @Input() control: FormControl;
+  @Input() text: string;
+
+  temErro(): boolean {
+    return this.control.hasError(this.error) && this.control.dirty;
+  }
+
+
+}
