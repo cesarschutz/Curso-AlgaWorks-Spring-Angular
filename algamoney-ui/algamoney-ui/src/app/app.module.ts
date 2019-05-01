e92a920fdf4c3b73552b6fdb2c60b9ca7b1c04e6
cesarschutz <cesarschutz340@gmail.com>
2019-05-01 18:41:11 -0300

    14.11 - Desafio criando o Core Module

--- algamoney-ui/algamoney-ui/src/app/app.module.ts
+++ algamoney-ui/algamoney-ui/src/app/app.module.ts
@@ -1,21 +1,21 @@
+import { CoreModule } from './core/core.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { PossoasModule } from './possoas/possoas.module';
 import { LancamentosModule } from './lancamentos/lancamentos.module';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    NavbarComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     LancamentosModule,
-    PossoasModule
+    PossoasModule,
+    CoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
